74f94cf29b402bceea66aebc6e26878c3374b1a8
Raymond Shum <rbshum@gmail.com>
2016-06-25 18:49:22 -0500

    Don't recalculate the path for refillers every tick
    Implemented states into refiller code, along with saving the path &
    target. Let's see if this works.

--- role.refiller.js
+++ role.refiller.js
@@ -2,45 +2,83 @@ var roleRefiller = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+
+    	// Creep.memory.state can have the following options for a refiller:
+    	// gettingEnergy -- triggers when energy = 0
+    	// refillingStructure -- triggers when energy is full
+
+    	if (!creep.memory.state) {
+    		// Set the initial state if it doesn't exist
+    		creep.memory.state = "gettingEnergy";
+    		creep.memory.path = null;
+    		creep.memory.target = null;
+    	}
 		
-		if(creep.carry.energy == 0) {
-            creep.memory.refilling = false;
+		if(creep.carry.energy == 0 && creep.memory.state != "gettingEnergy") {
+            creep.memory.state = "gettingEnergy" // Set the new state
+			creep.memory.path = null;	// Reset the path & target
+			creep.memory.target = null;
 	    }
-	    if(!creep.memory.refilling && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.refilling = true;
+	    else if(creep.memory.state != "refillingStructure" && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.state = "refillingStructure";
+			creep.memory.path = null;
+			creep.memory.target = null;
 	    }
-		
-		if(creep.memory.refilling) {
-	        var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
-	    }
-		
-        else {
-			var sources = creep.room.find(FIND_STRUCTURES, {
-				filter: (structure) => {
-					return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0);
-				}
-			});
-			
-			if (sources.length == 0) {
-				var sources = creep.room.find(FIND_SOURCES);
 				
-				if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+		if(creep.memory.state == "refillingStructure") {
+			
+			// We have a path, let's move.
+			if (creep.memory.path) {
+				if(creep.transfer(creep.memory.target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveByPath(creep.memory.path);
 				}
 			}
+			
+			// Else, let's find a spawn/extension to move to.
 			else {
-				if (sources[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(sources[0]);
+				
+				var targets = creep.room.find(FIND_STRUCTURES, {
+						filter: (structure) => {
+							return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+						}
+				});
+				if(targets.length > 0) {
+					
+					// Store the path in memory
+					creep.memory.path = creep.pos.findPathTo(targets[0]);
+					creep.memory.target = targets[0];				
+				}
+			}
+	    }
+		
+        else if (creep.memory.state == "gettingEnergy") {
+			
+			// We have a path, let's move
+			if (creep.memory.path) {
+				if (creep.memory.target.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveByPath(creep.memory.path);
+				}
+			}
+			
+			// Let's find a path to a container with energy
+			else {
+				var sources = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0);
+					}
+				});	
+				if (sources.length > 0) {
+					creep.memory.path = creep.pos.findPathTo(sources[0]);
+					creep.memory.target = sources[0];
+				}
+				else {
+					// Change the below emergency flag to true if we need the refiller to harvest as well.
+					var emergency = false;
+					if (emergency) {
+						var sources = creep.room.find(FIND_SOURCES);
+						creep.memory.target = sources[0];
+						creep.memory.path = creep.pos.findPathTo(sources[0]);
+					}
 				}
 			}
 	    }
