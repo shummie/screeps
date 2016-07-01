011d76425cd452047ef425614e91429234895e0a
Raymond Shum <rbshum@gmail.com>
2016-07-01 15:32:53 -0500

    Preparing for OOO branch merge
    Disable creation of new creeps.
    Run creeps with the new role names as well.

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var roleFixer = require('role.fixer');
 var roleRefiller = require('role.refiller');
 var roleFarMiner = require('role.farMiner');
 
+require('room');
+require('base');
+
 // Any modules that you use that modify the game's prototypes should be require'd
 // before you require the profiler.
 var profiler = require('screeps-profiler');
@@ -30,6 +33,7 @@ profiler.wrap(function() {
 
     var constructionSites = Game.spawns.Spawn1.room.find(FIND_CONSTRUCTION_SITES);
     
+    /*
 	if (Game.spawns.Spawn1.room.energyAvailable >= 550) {
 		if(miners.length < 1) {
 			var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'miner'});
@@ -43,7 +47,7 @@ profiler.wrap(function() {
 			var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'miner'});
 			console.log('Spawning new miner: ' + newName);
 		}
-		else if(refillers.length < 3) {
+		else if(refillers.length < 4) {
 			var newName = Game.spawns.Spawn1.createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'refiller'});
 			console.log('Spawning new refiller: ' + newName);
 		}
@@ -59,15 +63,15 @@ profiler.wrap(function() {
 			var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'fixer'});
 			console.log('Spawning new fixer: ' + newName);
 		}
-		else if(upgraders.length < 7) {
+		else if(upgraders.length < 9) {
 			var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'upgrader'});
 			console.log('Spawning new upgrader: ' + newName);
 		}
-	}
+	}*/
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'refiller') {
+        if(creep.memory.role == 'refiller' || creep.memory.role == "hauler") {
             roleRefiller.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
@@ -76,7 +80,7 @@ profiler.wrap(function() {
 		if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
-        if(creep.memory.role == 'miner') {
+        if(creep.memory.role == 'miner' || creep.memory.role == "harvester") {
             roleMiner.run(creep);
         }
         if(creep.memory.role == 'fixer') {
@@ -86,9 +90,38 @@ profiler.wrap(function() {
         	roleFarMiner.run(creep);
         }
     }
-
+    
+    
+    
     var tower = Game.getObjectById('576f0e7aed1407e029423934');
     if(tower) {
+     
+        
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+        
+        var target = tower.pos.findClosestByRange(Game.creeps, {
+			filter: function(t)
+			{
+				return t.hits < t.hitsMax
+			}
+		});
+		
+		if(target)
+		{
+			tower.heal(target);
+		}
+        
+    }
+    
+    var tower = Game.getObjectById('57749ad8c71a98f02faca782');
+    if(tower) {
+     
+        
+
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
             tower.attack(closestHostile);
