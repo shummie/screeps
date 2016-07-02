b671d6f2596261be7070d29b2dd11f658eef5926
Raymond Shum <rbshum@gmail.com>
2016-07-02 11:31:09 -0500

    allow upgraders to harvest own energy
    useful in new rooms

--- upgrader.js
+++ upgrader.js
@@ -8,12 +8,20 @@ class Upgrader extends Base {
 
 Upgrader.prototype.performRole = function() {
 	const empty = this.carry.energy === 0;
-	if (!empty) {
+	if (!empty && this.memory.task != "selfharvest") {
 		this.moveToAndUpgrade(this.room.controller);
+	} else if (this.carry.energy === this.carryCapacity && this.memory.task === "selfharvest") {
+		this.memory.task = undefined;
+		this.moveToAndUpgrade(this.room.controller);
+	} else if (empty && this.room.droppedControllerEnergy()) {
+    	this.takeEnergyFrom(this.room.droppedControllerEnergy());
 	} else if (empty && this.room.getStructuresWithEnergyPickup().length) {
 		const closestStruct = this.pos.findClosestByRange(this.room.getStructuresWithEnergyPickup());
 		this.takeEnergyFrom(closestStruct);
-	}	
+	} else {
+		this.memory.task = "selfharvest";
+		this.moveToAndHarvest(this.room.getSources()[0]);
+	}
 }
 
 
