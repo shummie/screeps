de58b74d3a2c449940543d67a77a0f9aef4cc10e
Raymond Shum <rbshum@gmail.com>
2016-07-01 21:08:03 -0500

    added various game functions

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function getFlagsOfType(type) {
 }
 
 
-var scoutFlags;
+var scoutFlags = undefined;
 
 const EnhancedGame = {};
 
@@ -18,6 +18,42 @@ EnhancedGame.roomArray = function() {
 	return Object.keys(Game.rooms).map(roomName => Game.rooms[roomName]);
 }
 
+EnhancedGame.clearScoutFlags = function() {
+    Game.getScoutFlags().forEach(flag => {
+    	flag.remove();
+    });
+}
 
+EnhancedGame.clearAllFlags = function () {
+    Game.flagArray().forEach(flag => {
+      	flag.remove();
+    });
+}
+
+EnhancedGame.getScoutFlags = function() {
+    if (scoutFlags === undefined) {
+    	scoutFlags = getFlagsOfType('scout');
+    }
+    return scoutFlags;
+}
+
+EnhancedGame.dismantleFlags = function() {
+    return getFlagsOfType('dismantle');
+}
+
+EnhancedGame.claimFlags = function() {
+    return getFlagsOfType('claim');
+}
+
+EnhancedGame.getClosestOwnedRoomTo = function(targetRoomName) {
+    if (!roomDistanceMap[targetRoomName]) {
+      	roomDistanceMap[targetRoomName] = Object.keys(Game.rooms).sort((roomNameA, roomNameB) => {
+        	const roomA = Game.rooms[roomNameA];
+        	const roomB = Game.rooms[roomNameB];
+        	return roomA.distanceToRoom(targetRoomName) - roomB.distanceToRoom(targetRoomName);
+      	})[0];
+    }
+    return roomDistanceMap[targetRoomName];
+}
 
 module.exports = EnhancedGame;
